e77b864b5df67b8873c03d053c03bf67eb045bcb
githubrenesoto <renesoto20@hotmail.com>
2021-10-27 21:24:53 -0400

    console.js modificado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('hola mundo con express!'))
+app.get('/', (req, res) => res.send('hola version 2'))
 
 app.listen(3000);
 console.log("server on port 3000")
